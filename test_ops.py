fe75cc787d945cb8a4462b775f219b14c2f9c234
Ganesh <ganeshkutty2257767@gmail.com>
2024-06-02 15:58:47 +0530

    Only add function

--- test_ops.py
+++ test_ops.py
@@ -4,17 +4,17 @@ import pytest
 def test_add_success():
     assert add(2, 2) == 4
     
-def test_sub_success():
-    assert sub(2, 2) == 0
+# def test_sub_success():
+#     assert sub(2, 2) == 0
 
-def test_mul_success():
-    assert mul(2, 2) == 4
+# def test_mul_success():
+#     assert mul(2, 2) == 4
 
-def test_mul_success_string():
-    assert mul('2', 2) == "22"
+# def test_mul_success_string():
+#     assert mul('2', 2) == "22"
 
-def test_div_success():
-    assert div(2, 2) == 1
+# def test_div_success():
+#     assert div(2, 2) == 1
 
 # Failure scenarios
 
@@ -22,18 +22,18 @@ def test_add_failure():
     with pytest.raises(TypeError):
         assert add('2', 2)
     
-def test_sub_failure():
-    with pytest.raises(TypeError):
-        assert sub('2', 2)
+# def test_sub_failure():
+#     with pytest.raises(TypeError):
+#         assert sub('2', 2)
 
-def test_mul_failure():
-    with pytest.raises(TypeError):
-        assert mul('2', '2')
+# def test_mul_failure():
+#     with pytest.raises(TypeError):
+#         assert mul('2', '2')
 
-def test_div_failure():
-    with pytest.raises(TypeError):
-        assert div('2', 2)
+# def test_div_failure():
+#     with pytest.raises(TypeError):
+#         assert div('2', 2)
 
-def test_div_zero():
-    with pytest.raises(ZeroDivisionError):
-        assert div(2, 0)
+# def test_div_zero():
+#     with pytest.raises(ZeroDivisionError):
+#         assert div(2, 0)
